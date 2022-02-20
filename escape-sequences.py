118e2b362f2daec9a969e094bdf2255a4d91a149
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-20 13:56:36 -0600

    Add files via upload

---
+++ escape-sequences.py
@@ -0,0 +1,26 @@
+#Escape Sequence
+weather = "It's sunny"
+print(weather)
+
+#Escape Sequence (doesn't work below)
+#weather = "It's "kind of" sunny"
+
+#Escape Sequence (doesn't work below)
+weather = "It\'s \"kind of\" sunny"
+
+print(weather)
+
+#Escape Sequence (blackslash)
+weather = "It\\s \"kind of\" sunny"
+
+print(weather)
+
+#Escape Sequence (blackslash t)
+weather = "\t It\\s \"kind of\" sunny"
+
+print(weather)
+
+#Escape Sequence (blackslash t and n)
+weather = "\t It\\s \"kind of\" sunny \n hope you have a good day!"
+
+print(weather)
